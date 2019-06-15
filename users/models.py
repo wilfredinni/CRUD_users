4afbe6baf7ebda8ecc6ba29f83a5be0acdb267c8
wilfredinni <carlos.w.montecinos@gmail.com>
2019-06-15 16:29:23 -0400

    profile

--- users/models.py
+++ users/models.py
@@ -1,9 +1,13 @@
 from django.contrib.auth.models import AbstractUser
+from django.urls import reverse
 from django.db import models
 
 
 class CustomUser(AbstractUser):
     bio = models.TextField(blank=True)
+
+    def get_absolute_url(self):
+        return reverse("profile", kwargs={"slug": self.username})
 
     def __str__(self):
         return self.username
